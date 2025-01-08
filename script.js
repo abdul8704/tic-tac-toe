85e299473e30f6a7db66ef35cf02872018c94e79
abdul8704 <abdulaziz87049@gmail.com>
2025-01-08 12:21:10 +0530

    done

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("single-player").addEventListener("click", () =>{
+    window.location.href = './tic-tac-toe-singlePlayer/single player index.html';
+});
+
+document.getElementById("multi-player").addEventListener("click", () =>{
+    window.location.href = './tic-tac-toe-multiPlayer/multi player index.html';
+})
